86fa2d3a65b5b7b369886ca78b72220fc954b858
maheshshinde <maheshshinde787@gmail.com>
2025-03-15 13:01:25 +0530

    fixing the prod issue of middle ware

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,24 @@
 import type { NextAuthConfig } from 'next-auth';
-import { NextResponse } from 'next/server';
- 
+
 export const authConfig = {
-    pages: {
-        signIn: '/login',
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
+  ],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
     },
-    callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
-          const isLoggedIn = !!auth?.user;
-          const isOnDashboard = nextUrl?.pathname.startsWith('/dashboard');
-            // Construct the base URL dynamically
-            const baseUrl =  process.env.AUTH_URL || nextUrl?.origin;
-            console.log("base",baseUrl);
-            
-          if (isOnDashboard) {
-            if (isLoggedIn) return true;
-            console.log("Not Logged In, Redirecting to Login:", baseUrl?.toString());
-            return false; // Redirect unauthenticated users to login page
-          } else if (isLoggedIn) {
-            console.log("User is Logged In, Redirecting to Dashboard:", baseUrl.toString());
-            return NextResponse.redirect(new URL('/dashboard', baseUrl));
-          }
-          return true;
-        },
-      },
-    providers: []
+  },
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,13 +1,14 @@
+
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
-import { z } from 'zod';
-import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
- 
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import { authConfig } from './auth.config';
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
@@ -17,7 +18,7 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
@@ -26,15 +27,17 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
+
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
+
           const user = await getUser(email);
           if (!user) return null;
+
           const passwordsMatch = await bcrypt.compare(password, user.password);
- 
           if (passwordsMatch) return user;
         }
+
         console.log('Invalid credentials');
         return null;
       },
