b3405eec0bcb76a9930b969af434ff527326200b
maheshshinde <maheshshinde787@gmail.com>
2025-03-15 12:45:00 +0530

    updated middleware to get env path

--- auth.config.ts
+++ auth.config.ts
@@ -8,19 +8,17 @@ export const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
           const isLoggedIn = !!auth?.user;
-          const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+          const isOnDashboard = nextUrl?.pathname.startsWith('/dashboard');
             // Construct the base URL dynamically
-            const baseUrl = `${nextUrl.protocol}//${nextUrl.host}`;
+            const baseUrl =  process.env.AUTH_URL || nextUrl?.origin;
             console.log("base",baseUrl);
             
           if (isOnDashboard) {
             if (isLoggedIn) return true;
-            console.log("noLoggedId", nextUrl)
-            console.log(new URL('/dashboard', nextUrl))
+            console.log("Not Logged In, Redirecting to Login:", baseUrl?.toString());
             return false; // Redirect unauthenticated users to login page
           } else if (isLoggedIn) {
-            console.log("isLoggedIN", nextUrl)
-            console.log('t ',new URL('/dashboard', baseUrl).toString())
+            console.log("User is Logged In, Redirecting to Dashboard:", baseUrl.toString());
             return NextResponse.redirect(new URL('/dashboard', baseUrl));
           }
           return true;
